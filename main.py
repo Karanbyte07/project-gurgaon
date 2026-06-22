820b70e2874bdc09e5561d9bf44c37e26e947a88
karanjeet kumar <karanbyte07@gmail.com>
2026-06-22 12:03:37 +0530

    selecting best model

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import cross_val_score
 
 
 # 1 -> Load the data
 
 housing_df = pd.read_csv("housing.csv")
 
-# 2-> create stratified shuffle split
 
+# 2-> create stratified shuffle split
 housing_df['income_category'] = pd.cut(housing_df['median_income'], 
                                        bins=[0,1.5,3.0,4.5,6.0, np.inf],
                                        labels=[1,2,3,4,5])
@@ -63,12 +68,45 @@ preprocessing_pipeline = ColumnTransformer(
 
 # 6 -> transform the data
 prepared_housing = preprocessing_pipeline.fit_transform(housing_features)
-print(prepared_housing.shape)
+# print(prepared_housing.shape)
 
 # 7 -> print the data after transformation
-final_housing = pd.DataFrame(
-    prepared_housing,
-    columns=preprocessing_pipeline.get_feature_names_out(),
-    index=train_housing_df.index,
-)
-print(final_housing)
+# final_housing = pd.DataFrame(
+#     prepared_housing,
+#     columns=preprocessing_pipeline.get_feature_names_out(),
+#     index=train_housing_df.index,
+# )
+# print(final_housing)
+
+# 8 -> Train the model (Selecting the best model using cross validation)
+
+#linear regression
+lin_reg = LinearRegression()
+lin_reg.fit(prepared_housing, housing_labels)
+lin_pred = lin_reg.predict(prepared_housing)
+#lin_rmse = root_mean_squared_error(housing_labels, lin_pred)
+#cross validation 
+lin_rmses = -cross_val_score(lin_reg, prepared_housing, housing_labels, scoring="neg_root_mean_squared_error", cv=10) 
+print(f"Linear Regression root mean squared error is : {lin_rmses.mean()}")
+
+
+#Decision Tree Regressor
+dt_reg = DecisionTreeRegressor()
+dt_reg.fit(prepared_housing, housing_labels)
+dt_pred = dt_reg.predict(prepared_housing)
+#dt_rmse = root_mean_squared_error(housing_labels, dt_pred)
+dt_rmses = -cross_val_score(dt_reg, prepared_housing, housing_labels, scoring="neg_root_mean_squared_error", cv=10)
+print(f"Decision Tree Regression root mean squared error is : {dt_rmses.mean()}")
+
+
+#Random Forest Regressor
+rf_reg = RandomForestRegressor()
+rf_reg.fit(prepared_housing, housing_labels)
+rf_pred = rf_reg.predict(prepared_housing)
+#rf_rmse = root_mean_squared_error(housing_labels, rf_pred)
+rf_rmses = -cross_val_score(rf_reg, prepared_housing, housing_labels, scoring="neg_root_mean_squared_error", cv=10)
+print(f"Random Forest Regression root mean squared error is : {rf_rmses.mean()}")
+
+
+#so the best model is Random Forest Regressor with the lowest root mean squared error. We will use this model to make predictions on the test set.
+
